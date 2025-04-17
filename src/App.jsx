014bb09d927f3dc0a650b30e5c5cc0af418eea04
CrimsonBambi <gerli.hirv@metropolia.fi>
2025-04-17 18:57:15 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,42 @@ import {BrowserRouter, Route, Routes} from 'react-router';
 import Home from './views/Home';
 import Layout from './components/Layout';
 import Login from './views/Login';
-import Logout from './views/Lougout';
+import Logout from './views/Logout';
 import Profile from './views/Profile';
 import Single from './views/Single';
 import Upload from './views/Upload';
+import {UserProvider} from './context/UserContext';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/upload" element={<Upload />} />
-          <Route path="/single" element={<Single />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/" element={<Home />} />
-        </Route>
-      </Routes>
+      <UserProvider>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/upload"
+              element={
+                <ProtectedRoute>
+                  <Upload />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/single" element={<Single />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="/" element={<Home />} />
+          </Route>
+        </Routes>
+      </UserProvider>
     </BrowserRouter>
   );
 }
--- src/views/Lougout.jsx
+++
@@ -1,6 +0,0 @@
-const Logout = () => {
-  // TODO: implement
-  return <p>Logout page here</p>;
-};
-
-export default Logout;
